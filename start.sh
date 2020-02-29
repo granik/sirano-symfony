55b551c6db190d274402303840b6d2a485d413ee
Alex <zzalexan@yandex.ru>
2020-02-29 15:23:05 +0300

    Fix bugs

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 
 # ./start.sh        выбираем Develope/Production и разворачиваем
 
+export COMPOSE_PROJECT_NAME=respiratory;
+
 # Develope вариант
 #export DOCKER_IMAGE_TAG=latest
 #export COMPOSE_FILE=base-dev.yml:env-dev.yml;
-#export COMPOSE_PROJECT_NAME=respiratory;
 
 # Production вариант
 export DOCKER_IMAGE_TAG=production
 export COMPOSE_FILE=base-prod.yml:env-prod.yml;
-export COMPOSE_PROJECT_NAME=respiratory;
 
 docker-compose stop && docker-compose up -d --build
